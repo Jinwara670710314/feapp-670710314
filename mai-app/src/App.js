40a019f04c0a89abbe40425ae50a95297f5d55e4
Jinwara Daungchang <daungchang_j@silpakorn.edu>
2026-08-20 05:12:51 +0000

    Lab React

--- mai-app/src/App.js
+++ mai-app/src/App.js
@@ -61,13 +61,15 @@
 
 // export default App;
 
-import './App.css';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
-import Footer from './components/Footer';
 
-import Greeting from './components/Greeting';
-import Card from './components/Card';
+// import './App.css';
+// import Header from './components/Header';
+// import MovieList from './components/MovieList';
+// import Footer from './components/Footer';
+
+// import Greeting from './components/Greeting';
+// import Card from './components/Card';
+
 
 // function App() {
 //   return (
@@ -82,29 +84,62 @@ import Card from './components/Card';
 //   );
 // }
 
-const students = [
-  { name: 'mai', year: 3 },
-  { name: 'gun', year: 2 },
-  { name: 'dew', year: 1 }
+// const students = [
+//   { name: 'mai', year: 3 },
+//   { name: 'gun', year: 2 },
+//   { name: 'dew', year: 1 }
+// ];
+
+// function App() {
+//   return (
+//     <div>
+
+//       <Card title="ประกาศ">
+//         <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
+//         <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
+//       </Card>
+
+//       <h1>รายชื่อนักศึกษา</h1>
+//       {students.map((s) => (
+//         <Greeting key={s.id} name={s.name} year={s.year} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import ProfileCard from './components/ProfileCard';
+import './App.css';
+
+const members = [
+  { id: 1, name: 'จิณห์วรา ด้วงช้าง', nickname: 'ไหม',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['โกโก้', 'หมา'] },
+  
+  { id: 2, name: 'ภานุกร กี่อนุกูล', nickname: 'กัน',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'หมา'] },
+  
+  { id: 3, name: 'จิราพัชร ด้วงช้าง', nickname: 'ดิว',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาไทย', 'แมว'] },
 ];
 
 function App() {
   return (
-    <div>
-
-      <Card title="ประกาศ">
-        <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
-        <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
-      </Card>
-
-      <h1>รายชื่อนักศึกษา</h1>
-      {students.map((s) => (
-        <Greeting key={s.id} name={s.name} year={s.year} />
-      ))}
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
