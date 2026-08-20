b49120a737d3108de9de191f8ba3d70068a819f7
Jinwara Daungchang <daungchang_j@silpakorn.edu>
2026-08-20 04:51:35 +0000

    First React App

---
+++ mai-app/src/App.js
@@ -0,0 +1,110 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+//function App() {
+//const name = 'Jinwara';
+//const year = 2026;
+
+//return (
+//<div>
+//<h1>สวัสดี {name}!</h1>
+//<p>ปีนี้คือปี {year} อีก 4 ปีจะเป็นปี {year + 4}</p>
+//<p>ตอนนี้เวลา {new Date().toLocaleTimeString('th-TH')}</p>
+//</div>
+//);
+//}
+
+//export default App;
+
+
+// import Hello from './Hello';  // 1. import เข้ามา (./ = โฟลเดอร์เดียวกัน)
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>แอปของฉัน</h1>
+//       <Hello />               {/* 2. ใช้เหมือนแท็ก HTML */}
+//       <Hello />               {/* 3. ใช้ซ้ำกี่ครั้งก็ได้! */}
+//       <Hello />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// function Header() {
+//   return <header><h1>🎬 MovieHub</h1></header>;
+// }
+
+// function MovieList() {
+//   return (
+//     <main>
+//       <p>รายการหนังจะมาอยู่ตรงนี้...</p>
+//     </main>
+//   );
+// }
+
+// function Footer() {
+//   return <footer><p>© 2026 วิชา 520 341</p></footer>;
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <MovieList />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import Footer from './components/Footer';
+
+import Greeting from './components/Greeting';
+import Card from './components/Card';
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <MovieList />
+//        <Greeting name="mai" year={3} />
+//        <Greeting name="gun" year={2} />
+//        <Greeting name="dew" year={1} />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+const students = [
+  { name: 'mai', year: 3 },
+  { name: 'gun', year: 2 },
+  { name: 'dew', year: 1 }
+];
+
+function App() {
+  return (
+    <div>
+
+      <Card title="ประกาศ">
+        <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
+        <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
+      </Card>
+
+      <h1>รายชื่อนักศึกษา</h1>
+      {students.map((s) => (
+        <Greeting key={s.id} name={s.name} year={s.year} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+
