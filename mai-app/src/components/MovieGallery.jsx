74ff07febc0ae3fc8bec6bc00226c9210241adf1
Jinwara Daungchang <daungchang_j@silpakorn.edu>
2026-09-10 05:28:35 +0000

    newdata & state

--- mai-app/src/components/MovieGallery.jsx
+++ mai-app/src/components/MovieGallery.jsx
@@ -36,7 +36,6 @@ function MovieGallery() {
       )}
       </div>
 
-
       <h2 className="mt-4 text-2xl font-bold text-slate-800">
         {movie.title} <span className="text-slate-400">({movie.year})</span>
       </h2>
